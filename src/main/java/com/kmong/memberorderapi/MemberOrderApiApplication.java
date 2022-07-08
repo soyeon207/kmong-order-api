16dd3b2df7a2a76dd6cd6b78c98d43abc38e6b42
psy010207@naver.com <psy010207@naver.com>
2022-07-08 23:06:35 +0900

    Feat: 초기프로젝트 세팅 (#1)

---
+++ src/main/java/com/kmong/memberorderapi/MemberOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.kmong.memberorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberOrderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmong/memberorderapi/MemberOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmong.memberorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
